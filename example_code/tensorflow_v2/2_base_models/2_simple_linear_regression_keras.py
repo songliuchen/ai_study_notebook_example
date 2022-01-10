1bfee1fe05732bf02daa59ad854f4f2903e1b788
songliuchen <song_gis@163.com>
2022-01-10 08:45:51 +0800

    添加线性回归examples上传

--- example_code/tensorflow_v2/2_base_models/2_simple_linear_regression_keras.py
+++ example_code/tensorflow_v2/2_base_models/2_simple_linear_regression_keras.py
@@ -15,11 +15,9 @@ import os
 # 设置日志级别0 全部，1提示，2警告，3错误
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = '3'
 
-
 # 读取训练集和测试集
 def read_data(item):
     return {"x": float(item.split(",")[0]), "y": float(item.split(",")[1])}
-
 
 train_data = read_file(path="../../data/simple_linear_data_train.txt", custon_convert=read_data)
 test_data = read_file(path="../../data/simple_linear_data_test.txt", custon_convert=read_data)
@@ -41,31 +39,48 @@ for item in test_data:
     test_data_arr_x.append((item["x"]))
     test_data_arr_y.append((item["y"]))
 
-
+#总迭代次数
+epouchs = 5
+#单批次大小
+batch_size = 100
+#预热比例,添加预热比例，避免一开始loss值过s大，loss曲线效果不明显
+wrap_rate = 0.01
+#计算总训练步数
+global_steps = epouchs*(len(train_data)/batch_size)
+#根据预热比例计算预热步数
+wrap_step = int(global_steps*wrap_rate)
+# 保存训练集损失值
+loss_train_x = []
+loss_train_y = []
+# 保存测试集损失值
+loss_test_x = []
+loss_test_y = []
 class LossHistory(tf.keras.callbacks.Callback):
     def __init__(self, model):
-        self.current_index = 0
         self.model = model
+        self.current_index = 0
 
-    def on_batch_start(self, batch, logs={}):
-        print("dddddddddddddddddddddddddd" + str(batch))
-        self.current_index += 1
-        current_index = self.current_index
-        # if current_index % 100 == 0:
-        #     print("train loss:%s" % self.model)
-        #     # 获取当前模型预测 k，b的值
-        #     print("k_val:%s b_val:%s" % (w.numpy(), b.numpy()))
-        #     loss_train_x.append(len(loss_train_x))
-        #     loss_train_y.append(loss)
-        #
-        #     # 测试集部分
-        #     x.assign(test_data_arr_x)
-        #     y.assign(test_data_arr_y)
-        #     loss_test_x.append(len(loss_train_x) - 1)
-        #
-        #     loss_test_y.append(loss)
-        #     print("test loss:%s" % loss.numpy())
-        #     print("")
+    def on_train_batch_end(self, batch, logs={}):
+        self.current_index +=1
+        if self.current_index % 100 == 0 and self.current_index > wrap_step:
+            print("train loss:%s" % logs["loss"])
+            loss_train_x.append(len(loss_train_x))
+            loss_train_y.append(logs["loss"])
+
+    def on_test_batch_end(self, batch, logs={}):
+        if self.current_index % 100 == 0 and self.current_index > wrap_step:
+            print("test loss:%s" % logs["loss"])
+            weight = model.get_weights()
+            print("k_val:%s b_val:%s" % (weight[0], weight[1]))
+            print("")
+            loss_test_x.append(len(loss_train_x) - 1)
+            loss_test_y.append(logs["loss"])
+
+    def on_epoch_end(self,epoch, logs=None):
+        if epoch == epouchs:
+            draw_line([{"x": loss_train_x, "y": loss_train_y, "name": "train_loss", "color": "blue"},
+                       {"x": loss_test_x, "y": loss_test_y, "name": "test_loss", "color": "red"}],
+                      {"x_name": "step", "y_name": "loss", "title": "loss/step"})
 
 
 def create_model():
@@ -86,7 +101,7 @@ optimizer = tf.keras.optimizers.SGD(learning_rate=0.1)
 model.compile(optimizer=optimizer, loss='mse')
 
 # 进行模型训练
-model.fit(train_data_arr_x, train_data_arr_y, epochs=5, batch_size=100, validation_steps=100,
+model.fit(train_data_arr_x, train_data_arr_y,verbose=0, epochs=epouchs, batch_size=100, validation_steps=100,
           validation_data=(test_data_arr_x, test_data_arr_y), callbacks=[LossHistory(model)])
 
 # 保存模型
@@ -102,9 +117,6 @@ checkpoint_new = tf.train.Checkpoint(linear_model=new_model)
 checkpoint_new.restore(tf.train.latest_checkpoint(saved_folder))
 test_y = new_model.predict(test_data_arr_x)
 
-# # 绘制loss折线图
-# draw_line([{"x":loss_train_x,"y":loss_train_y,"name":"train_loss","color":"blue"},{"x":loss_test_x,"y":loss_test_y,"name":"test_loss","color":"red"}],{"x_name":"step","y_name":"loss","title":"loss/step"})
-#
 # 绘制离散点效果图
 draw_data = [{"x": test_data_arr_x, "y": test_data_arr_y, "name": "real_val", "type": "point", "color": "blue"},
              {"x": test_data_arr_x, "y": test_y, "name": "pre_val", "type": "line", "color": "red"}]
